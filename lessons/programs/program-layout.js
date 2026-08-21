1165f2be0440605a85a2014368606da890ca0f26
orcungungor9-creator <orcungungor9@gmail.com>
2026-08-21 19:41:45 +0300

    Lessons klasörüne içerik eklendi

---
+++ lessons/programs/program-layout.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const accordions = document.querySelectorAll('.accordion-header');
+    accordions.forEach(acc => {
+        acc.addEventListener('click', () => {
+            const item = acc.parentElement;
+            const content = item.querySelector('.accordion-content');
+            
+            if (item.classList.contains('active')) {
+                item.classList.remove('active');
+                content.style.maxHeight = null;
+            } else {
+                document.querySelectorAll('.accordion-item.active').forEach(other => {
+                    other.classList.remove('active');
+                    other.querySelector('.accordion-content').style.maxHeight = null;
+                });
+                
+                item.classList.add('active');
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    });
+});
